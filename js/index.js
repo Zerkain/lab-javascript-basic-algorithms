702a085775ceea9b07d296b2cd4a1222d633164f
Alejandro Rodriguez <aperturegir@gmail.com>
2023-07-13 07:42:17 +0100

    2done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
+let hacker1;
+let hacker2;
 
+hacker1 = "Alex"
+hacker2= "Michael"
 
+console.log(`The Drivers Name is ${hacker1}`)
+console.log(`The Navigators Name is ${hacker2}`)
 // Iteration 2: Conditionals
+
+if(hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name it has ${hacker1.length} characters!`)
+}else if(hacker1.length < hacker2.length){
+    console.log(`It seems that the navigator has the longest name it has ${hacker2.length} characters`)
+}else {
+    console.log(`wow! you both equally have long names! ${hacker1.length} characters!!!`)
+}
 
 
 // Iteration 3: Loops
